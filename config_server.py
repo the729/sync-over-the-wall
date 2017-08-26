a941ec07af491784e3545cb299d72801595d3aa9
tianji <tj@hhttech.com>
2017-08-26 23:03:46 +0800

    remove some outputs.
    add block diagrams

--- config_server.py
+++ config_server.py
@@ -3,7 +3,7 @@ import ssl
 import patch_hosts
 import threading
 
-BLOG_URL = "http://blog.wutj.info/"
+BLOG_URL = "README.md"
 
 class ConfigHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -21,23 +21,23 @@ def main():
     print "Please refer to %s for detailed instructions."  % BLOG_URL
 
     # Step 1
-    print "\r\nStep 1: patch hosts file (Admin required)...",
+    #print "\r\nStep 1: patch hosts file (Admin required)...",
     if not patch_hosts.sync_patch_hosts(): return False
-    print "Done"
+    #print "Done"
 
     # Step 2
-    print "\r\nStep 2: start fake config.resilio.com server..."
+    #print "\r\nStep 2: start fake config.resilio.com server..."
     httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 443), ConfigHandler)
     httpd.socket = ssl.wrap_socket (httpd.socket, keyfile='./do_not_trust.key', certfile='./do_not_trust.crt', server_side=True)
-    print "Now, please restart Sync."
+    print "Fake server started. Now, please restart Sync."
     print "After Sync has requested the config file, this program should go on automatically."
     print "If this program blocks here, please refer to %s" % BLOG_URL
     httpd.serve_forever()
 
     # Step 3
-    print "\r\nStep 3: restore hosts file (Admin required)...",
+    #print "\r\nStep 3: restore hosts file (Admin required)...",
     if not patch_hosts.sync_unpatch_hosts(): return False
-    print "Done"
+    #print "Done"
 
     return True
 
